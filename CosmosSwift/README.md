52b18bf1e5e883f55fc7e68e86c8a9b7affae640
Aaron Craelius <aaronc@users.noreply.github.com>
2021-03-30 13:46:33 -0400

    Remove poc code

--- CosmosSwift/README.md
+++
@@ -1,3 +0,0 @@
-# CosmosSwift
-
-A description of this package.
--- CosmosSwift/Tests/LinuxMain.swift
+++
@@ -1,7 +0,0 @@
-import XCTest
-
-import CosmosSwiftTests
-
-var tests = [XCTestCaseEntry]()
-tests += CosmosSwiftTests.allTests()
-XCTMain(tests)
