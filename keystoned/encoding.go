7646b7d1ebe20e418aa8889394b3c04d1b937591
John Kemp <stable.pseudonym@gmail.com>
2021-08-13 15:35:41 -0400

    made method private

--- keystoned/encoding.go
+++ keystoned/encoding.go
@@ -4,16 +4,25 @@ import (
 	"github.com/cosmos/cosmos-sdk/codec"
 	"github.com/cosmos/cosmos-sdk/codec/types"
 	"github.com/cosmos/cosmos-sdk/simapp/params"
-	"github.com/cosmos/cosmos-sdk/std"
+	//"github.com/cosmos/cosmos-sdk/std"
 	"github.com/cosmos/cosmos-sdk/x/auth/tx"
+	grptypes "github.com/regen-network/regen-ledger/x/group"
+	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
+	cryptotypes "github.com/cosmos/cosmos-sdk/crypto/codec"
 )
 
 // MakeEncodingConfig creates an EncodingConfig
-func MakeEncodingConfig() params.EncodingConfig {
+// registering all of the types that are needed
+// for the caller to call the APIs it does
+func makeEncodingConfig() params.EncodingConfig {
 	amino := codec.NewLegacyAmino()
 	interfaceRegistry := types.NewInterfaceRegistry()
 	marshaler := codec.NewProtoCodec(interfaceRegistry)
 	txCfg := tx.NewTxConfig(marshaler, tx.DefaultSignModes)
+
+	authtypes.RegisterInterfaces(interfaceRegistry)
+	cryptotypes.RegisterInterfaces(interfaceRegistry)
+	grptypes.RegisterTypes(interfaceRegistry)
 
 	encodingConfig := params.EncodingConfig{
 		InterfaceRegistry: interfaceRegistry,
@@ -21,7 +30,6 @@ func MakeEncodingConfig() params.EncodingConfig {
 		TxConfig:          txCfg,
 		Amino:             amino,
 	}
-	std.RegisterLegacyAminoCodec(encodingConfig.Amino)
-	std.RegisterInterfaces(encodingConfig.InterfaceRegistry)
+	
 	return encodingConfig
 }
