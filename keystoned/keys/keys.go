e6f63b64cceabd98d2e42a72d3123867d02bb694
John kemp <john.kemp@regen.network>
2021-08-26 11:05:56 -0700

    corrected Address function and added Sign

--- keystoned/keys/keys.go
+++ keystoned/keys/keys.go
@@ -1,10 +1,12 @@
 package keys
 
 import (
+	"log"
 	"crypto"
 	"crypto/ecdsa"
 	"crypto/elliptic"
 	"crypto/sha256"
+	"crypto/rand"
 	"golang.org/x/crypto/ripemd160"
 
 	"github.com/frumioj/crypto11"
@@ -42,6 +44,7 @@ type CryptoPrivKey interface {
 
 // CryptoPubKey looks a lot like a tmcrypto-inherited
 // PubKey, but is not defined in a protobuf message
+// See cosmos-sdk/crypto/keys/internal/ecdsa/pubkey.go for inspiration
 type CryptoPubKey struct {
 	crypto.PublicKey
 
@@ -65,15 +68,30 @@ func (pk CryptoKey) Bytes() []byte {
 	return []byte{}
 }
 
-func (pk CryptoKey) Sign(msg []byte) ([]byte, error) {
-	return []byte{}, nil
+func (pk CryptoKey) Sign(plaintext []byte, hashFun *crypto.Hash ) ([]byte, error) {
+
+	if hashFun != nil {
+
+		h := hashFun.New()
+		_, err := h.Write(plaintext)
+		digest := h.Sum(nil)
+		
+		if err != nil {
+			log.Printf("Error creating digest %s", err.Error())
+			return []byte{}, nil
+		}
+		
+		return pk.signer.Sign(rand.Reader, digest[:], nil)
+	} else {
+		return pk.signer.Sign(rand.Reader, plaintext, nil)
+	}
 }
 
 func (pk CryptoKey) Equals(other CryptoKey) bool {
 	return true
 }
 
-func (pk CryptoKey) PubKey() CryptoPubKey { return CryptoPubKey{PublicKey: pk.signer.Public }}
+func (pk CryptoKey) PubKey() *CryptoPubKey { return &CryptoPubKey{pk.signer.Public(), nil }}
 
 func (pk CryptoKey) Type() string { return "CryptoKey" }
 
@@ -81,20 +99,38 @@ func (pk CryptoKey) Delete() error { return pk.signer.Delete() }
 
 func (pk CryptoKey) Public() crypto.PublicKey { return pk.signer.Public() }
 
-func (pubKey *CryptoPubKey) MarshalPublicKeyToAddress() tmcrypto.Address {
-
+func (pubKey *CryptoPubKey) Bytes() []byte {
 	switch pub := pubKey.PublicKey.(type) {
 	case *ecdsa.PublicKey:
-		// @@ TODO: currently does the btc secp256k1 transform
-		// but should also support r1, by looking first at
-		// curve params - switch inside a switch
-		publicKeyBytes := elliptic.MarshalCompressed(pub.Curve, pub.X, pub.Y)
-		sha := sha256.Sum256(publicKeyBytes)
-		hasherRIPEMD160 := ripemd160.New()
-		hasherRIPEMD160.Write(sha[:]) // does not error
-		return tmcrypto.Address(hasherRIPEMD160.Sum(nil))
+		return elliptic.MarshalCompressed(pub.Curve, pub.X, pub.Y)
 	default:
-		panic("Unsupported public key!")
+		panic("Unsupported public key type!")
+	}
+}
+
+// Address takes a CryptoPubKey, expecting that it has
+// a crypto.PublicKey base struct, marshals the struct into bytes using
+// ANSI X.
+func (pubKey *CryptoPubKey) Address() tmcrypto.Address {
+
+	if pubKey.address == nil {
+		switch pub := pubKey.PublicKey.(type) {
+		case *ecdsa.PublicKey:
+			// @@ TODO: currently does the btc secp256k1 transform
+			// but should also support r1, by looking first at
+			// curve params - switch inside a switch
+			publicKeyBytes := pubKey.Bytes()
+			sha := sha256.Sum256(publicKeyBytes)
+			hasherRIPEMD160 := ripemd160.New()
+			hasherRIPEMD160.Write(sha[:]) // does not error
+			pubKey.address = tmcrypto.Address(hasherRIPEMD160.Sum(nil))
+			return pubKey.address
+		default:
+			log.Printf("Type: %T", pub)
+			panic("Unsupported public key!")
+		}
+	} else {
+		return pubKey.address
 	}
 
 }
